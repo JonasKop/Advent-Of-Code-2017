f9170267fb819d8c80a03612aa91893ce6994c42
Jonaskop <jonas.k.sjodin@gmail.com>
2017-12-17 13:46:43 +0100

    Added files

---
+++ Day-1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char *argv[]) {
+
+    std::string input;
+    std::ifstream input_file(argv[1]);
+
+    if (input_file.is_open()) {
+        getline(input_file, input);
+        input_file.close();
+    } else {
+        std::cout << "Unable to open file\n";
+        exit(1);
+    }
+
+    int sum = 0;
+    for (unsigned int i = 0; i < input.size() - 1; ++i) {
+
+        int c0 = input.at(i) - '0';
+        int c1 = input.at(i + 1) - '0';
+
+        if (c0 == c1) {
+            sum += c0;
+        }
+    }
+
+    if (input.at(input.size() - 1) == input.at(0)) {
+        int c0 = input.at(input.size() - 1) - 48;
+        int c1 = input.at(0) - 48;
+
+        if (c0 == c1) {
+            sum += c0;
+        }
+    }
+
+    std::cout << sum << std::endl;
+    return 0;
+}
