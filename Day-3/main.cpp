89e7301b534c7d15acd39121e3ac4619148d8ddd
Jonaskop <jonas.k.sjodin@gmail.com>
2017-12-22 23:32:41 +0100

    Finished Part 2

--- Day-3/main.cpp
+++ Day-3/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
 
-int getlength(int size, int maxnr) {
+
+int getLength(int size, int maxnr) {
     int length = 1;
     int i = 1;
     int x = size / 2;
     int y = x;
+
 
     while (true) {
         ++x;
@@ -16,6 +19,7 @@ int getlength(int size, int maxnr) {
         while (y > temp_y - length) {
             --y;
             ++i;
+
             if (i == maxnr) {
                 return abs(x - size / 2) + abs(y - size / 2);
             }
@@ -26,6 +30,7 @@ int getlength(int size, int maxnr) {
         while (x > temp_x - length) {
             --x;
             ++i;
+
             if (i == maxnr) {
                 return abs(x - size / 2) + abs(y - size / 2);
             }
@@ -35,6 +40,7 @@ int getlength(int size, int maxnr) {
         while (y < temp_y + length) {
             ++y;
             ++i;
+
             if (i == maxnr) {
                 return abs(x - size / 2) + abs(y - size / 2);
             }
@@ -44,12 +50,99 @@ int getlength(int size, int maxnr) {
         while (x < temp_x + length) {
             ++x;
             ++i;
+
             if (i == maxnr) {
                 return abs(x - size / 2) + abs(y - size / 2);
             }
         }
         ++length;
     }
+}
+
+int near(std::vector<std::vector<int>> vec, int x, int y) {
+
+    int sum = 0;
+
+    for (int i = -1; i < 2; ++i) {
+        sum += vec[x + i][y - 1];
+        sum += vec[x + i][y + 1];
+    }
+    sum += vec[x - 1][y];
+    sum += vec[x + 1][y];
+
+    return sum;
+}
+
+int getstrangelength(int maxnr){
+    int size = 15;
+    int x = size / 2;
+    int y = x;
+    std::vector<std::vector<int>> vec;
+    int val = 1;
+    int length = 1;
+
+
+    for (int i = 0; i < size; ++i) {
+        std::vector<int> v;
+        for (int j = 0; j < size; ++j) {
+            v.push_back(0);
+        }
+        vec.push_back(v);
+    }
+
+    vec[x][y] = val;
+    int i = 1;
+    while (i < (13 * 13)) {
+
+        ++x;
+        ++i;
+        vec[x][y] = near(vec, x, y);
+        int temp_y;
+        int temp_x;
+
+        temp_y = y;
+        while (y > temp_y - length) {
+            --y;
+            ++i;
+            vec[x][y] = near(vec, x, y);
+            if (vec[x][y] > maxnr) {
+                return vec[x][y];
+            }
+        }
+
+        ++length;
+        temp_x = x;
+        while (x > temp_x - length) {
+            --x;
+            ++i;
+            vec[x][y] = near(vec, x, y);
+            if (vec[x][y] > maxnr) {
+                return vec[x][y];
+            }
+        }
+
+        temp_y = y;
+        while (y < temp_y + length) {
+            ++y;
+            ++i;
+            vec[x][y] = near(vec, x, y);
+            if (vec[x][y] > maxnr) {
+                return vec[x][y];
+            }
+        }
+
+        temp_x = x;
+        while (x < temp_x + length) {
+            ++x;
+            ++i;
+            vec[x][y] = near(vec, x, y);
+            if (vec[x][y] > maxnr) {
+                return vec[x][y];
+            }
+        }
+        ++length;
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -64,7 +157,8 @@ int main(int argc, char *argv[]) {
 
     for (size = 1; size * size < maxnr; size += 2);
 
-    std::cout << getlength(size, maxnr) << std::endl;
-    
+    std::cout << "Part 1: " << getLength(size, maxnr) << std::endl;
+    std::cout << "Part 2: " << getstrangelength(maxnr) << std::endl;
+
     return 0;
 }
